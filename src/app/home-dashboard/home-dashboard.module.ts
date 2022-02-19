fecaf1e26d3e9ceabc71dfa6e054a953387a6750
Huỳnh Bảo <huynhbao162000@gmail.com>
2022-02-19 21:28:23 +0700

    update

--- src/app/home-dashboard/home-dashboard.module.ts
+++ src/app/home-dashboard/home-dashboard.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ManageUserComponent } from './manage-user/manage-user.component';
 import { RouterModule } from '@angular/router';
 import { HomeDashboardRoutes } from './home-dashboard-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -11,6 +12,7 @@ import { HomeDashboardRoutes } from './home-dashboard-routing.module';
   ],
   imports: [
     CommonModule,
+    NgbModule,
     RouterModule.forChild(HomeDashboardRoutes),
   ]
 })
